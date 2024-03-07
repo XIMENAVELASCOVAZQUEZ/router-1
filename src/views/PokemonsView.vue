b850a5282d8f53e2a95842c4c42c601eb347d946
Ximena <ximena.velasco@cceo.com.mx>
2024-03-07 10:54:15 -0600

    feat: :sparkles: Axios

--- src/views/PokemonsView.vue
+++ src/views/PokemonsView.vue
@@ -1,3 +1,22 @@
+<script setup>
+
+import { ref } from "vue";
+import axios from "axios";
+
+const pokemons = ref([]);
+
+const getData = async () => {
+    try {
+        const { data }  = await axios.get("https://pokeapi.co/api/v2/pokemon");
+        console.log(data.results);
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+getData();
+</script>
+
 <template>
     <h1>Pokemons</h1>
 </template>
